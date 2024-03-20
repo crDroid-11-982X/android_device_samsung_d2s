a8016cb531b899434de47b57f41ab88c9face552
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    d2s: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- lineage_d2s.mk
+++ lineage_d2s.mk
@@ -39,9 +39,6 @@ TARGET_SCREEN_WIDTH := 1440
 ## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_d2s
 PRODUCT_DEVICE := d2s
